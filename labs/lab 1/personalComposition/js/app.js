1b15691be0b590be3e4086958d2ba509ca0a5191
aibreich <aibreich@iu.edu>
2021-08-31 14:04:11 -0400

    Uploaded Personal Composition

---
+++ labs/lab 1/personalComposition/js/app.js
@@ -0,0 +1,60 @@
+function setup() {
+    var red = "#000000";
+    var ted = "#a9a9a9";
+    var blew = "#00FFFF"
+    var startX = 30;
+
+    createCanvas (800, 800);
+    // circle( 50, 50, 40 );
+    // rect(300, 300, 300, 30);
+
+    noStroke();
+
+    background(ted);
+    fill(111, 208, 232);
+    // rect(startX, 60, 300, 150);
+
+    fill(ted);
+    square(150, 150, 450);
+    
+
+    fill(blew);
+    square(200, 200, 350);
+
+    fill(ted);
+    circle(200, 200, 150);
+    circle(550, 200, 150);
+    circle(200, 550, 150);
+    circle(550, 550, 150);
+    circle(375, 375, 150);
+    
+
+
+    fill(blew);
+    circle(200, 200, 100);
+    circle(550, 200, 100);
+    circle(200, 550, 100);
+    circle(550, 550, 100);
+    circle(375, 375, 100);
+    
+    fill(ted)
+    circle(287, 288, 148);
+    circle(287, 462, 148);
+    circle(463, 288, 148);
+    circle(463, 462, 148);
+    circle(140, 375, 150);
+    circle(610, 375, 150);
+    circle(375, 140, 150);
+    circle(375, 610, 150);
+
+    fill(blew)
+    circle(287, 288, 96);
+    circle(287, 462, 96);
+    circle(463, 288, 96);
+    circle(463, 462, 96);
+    circle(140, 375, 96);
+    circle(610, 375, 96);
+    circle(375, 140, 96);
+    circle(375, 610, 96);
+
+}
