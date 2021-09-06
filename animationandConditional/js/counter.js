7a6a095ef7cf16a775e573d71044522d7c82d673
aibreich <aibreich@iu.edu>
2021-09-06 12:06:00 -0400

    fixed counter hopefully

--- animationandConditional/js/counter.js
+++ animationandConditional/js/counter.js
@@ -1,6 +1,15 @@
 let counter = 0;
+let xPos = 0;
+function setup() {
+    createCanvas(800, 600);
+
+}
 function draw() {
+    background(0)
+    xPos = xPos + 1;
     counter = counter + 1;
-    console.log("This is my counter" + counter);
+    console.log(xPos + counter);
+    fill(66, 120, 245)
+    circle(xPos, 50, 45)
 }
 
